8b2c58b574bda57762a6cba8aaca58465f3dc6dc
Elomina Zivv <zivv_elomina@dlsu.edu.ph>
2026-07-14 16:10:15 +0800

    Window

--- JAZZY/Source/JAZZY/Cube.cpp
+++
@@ -1,5 +0,0 @@
-#include <JAZZY/Cube.h>
-
-jazzy::Cube::Cube(Vec3 _position, Vec3 _scale):position(_position), scale(_scale)
-{
-}
---
+++ JAZZY/Source/JAZZY/Window/Win32/Win32Window.cpp
@@ -0,0 +1,114 @@
+#include <JAZZY/Window/Window.h>
+#include <Windows.h>
+#include <stdexcept>
+
+#include "JAZZY/Core/Logger.h"
+#include "JAZZY/Core/Logger.h"
+
+static LRESULT CALLBACK WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	// DefWindowProc Default Window Procedure does not assume that the programs ends just because the window is closed
+	// Which is why the program has to modify the windowprocedure to 
+	switch (msg)
+	{
+		case WM_CLOSE:
+		{
+			PostQuitMessage(0);
+
+			break;
+		}
+		// Experiment Lol
+		// These are Window Messages. kewl
+		case WM_KEYDOWN:
+		{
+				if (wparam == 'F')
+				{
+					SetWindowTextA(hwnd, "You key DOWN 'F' ");
+				}
+				break;
+		}
+		case WM_KEYUP:
+		{ 
+			if (wparam == 'F')
+			{
+				SetWindowTextA(hwnd, "You key UP 'F' ");
+			}
+			break;
+		}
+
+		default:
+			return DefWindowProc(hwnd, msg, wparam, lparam);
+	}
+	return 0;
+}
+
+
+
+jazzy::Window::Window(const WindowDesc& desc): Base(desc.base), m_size(desc.size)
+{
+	// In Windowese, the Window's windows have an internal class and internal instances
+	// RegisterWindowClassFunction creates the Window's internal window class
+	auto registerWindowClassFunction = []()
+		{
+			WNDCLASSEX wc{}; // Brace Initialization
+			wc.cbSize = sizeof(WNDCLASSEX);
+			wc.lpszClassName = L"JAZZYWindow";
+			wc.lpfnWndProc = &WindowProcedure;
+			return RegisterClassEx(&wc);
+		};
+
+	static const auto windowClassId = std::invoke(registerWindowClassFunction);
+
+	if (!windowClassId)
+		DX3DLogThrowError("RegisterClassEx failed.");
+
+	RECT rc{ 0,0,m_size.width,m_size.height };
+	AdjustWindowRect(
+		&rc,
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
+		false
+	);
+
+	// I guess this one makes a Window's internal instance of the window class made earlier
+	m_handle = CreateWindowEx(
+		NULL,
+		MAKEINTATOM(windowClassId),
+		L"ELOMINA | JAZZY Game Engine", 
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		rc.right - rc.left,
+		rc.bottom - rc.top,
+		NULL,
+		NULL,
+		NULL,
+		NULL
+	);
+
+	if (!m_handle)
+		DX3DLogThrowError("CreateWindowEx failed.");
+
+	ShowWindow(static_cast<HWND>(m_handle), SW_SHOW);
+}
+
+jazzy::Window::~Window()
+{
+	DestroyWindow(static_cast<HWND>(m_handle));
+}
+
+jazzy::Rect jazzy::Window::getClientAreaInScreenSpace()
+{
+	auto hwnd = static_cast<HWND>(m_handle);
+	RECT client{};
+	// Retrieves the client window rect relative to the upper-left corner of a window's client area at (0,0)
+	GetClientRect(hwnd, &client);
+
+	POINT topLeft{ client.left, client.top };
+	POINT bottomRight{ client.right, client.bottom };
+	// The ClientToScreen function converts the client-area coordinates of a specified point to screen coordinates.
+	ClientToScreen(hwnd, &topLeft);
+	ClientToScreen(hwnd, &bottomRight);
+
+	return { topLeft.x, topLeft.y, bottomRight.x - topLeft.x, bottomRight.y - topLeft.y };
+
+}
